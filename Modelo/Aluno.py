9154176d9f1950cd56352cfa65a2d099fa6c2c00
vitoriapiloto <vitoriapiloto477@gmail.com>
2024-09-30 20:28:50 -0300

    Commit inicial

---
+++ Modelo/Aluno.py
@@ -0,0 +1,12 @@
+class Aluno(db.Model):
+   identificador = db.Column(db.int, primary_key=True)
+   nome = db.Column(db.String(50))
+   nota = db.Column(db.String(50))
+
+   @property
+   def as_json(self):
+       return {
+           'id': self.identificador,
+           'nome': self.nome,
+           'nota': self.nota
+       }
