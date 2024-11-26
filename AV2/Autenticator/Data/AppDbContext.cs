b1c05bb3f3853253c173d9f6517d6d032f645c58
Vitória Piloto <vitoriapiloto477@gmail.com>
2024-11-26 20:54:54 -0300

    Implementação do OTP (2FA)

--- AV2/Autenticator/Data/AppDbContext.cs
+++ AV2/Autenticator/Data/AppDbContext.cs
@@ -7,6 +7,7 @@ public class AppDbContext : DbContext
     public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
 
     public DbSet<User> Usuarios { get; set; }
+    public DbSet<_2FA> TwoFactor { get; set; }
     public DbSet<SecurityLog> LogsSeguranca { get; set; }
     public DbSet<Blacklist> BlacklistTokens { get; set; }
 }
